29e2b5ca275c03fbd1a87c7d0d85f0bff7e2cb35
Seth Lesky <sethlesky@gmail.com>
2015-10-05 17:17:35 -0600

    form

--- week6/server/routes/index.js
+++ week6/server/routes/index.js
@@ -11,6 +11,26 @@ fs.readFile('data.txt', function(err, data){
     });
 });
 
+router.get('/form', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.header('Content-Type', 'text/html');
+  res.render('form', { title: 'Form'} );
+});
+
+router.get('/success', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.header('Content-Type', 'text/html');
+  // res.render('form', { title: 'Form'} );
+  res.send('wooooooop wooooop');
+});
+
+
+router.post('/formsubmit', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  // res.header('Content-Type', 'text/html');
+  // res.render('form', { title: 'Form'} );
+  res.redirect('/success')
+});
 
 
 
