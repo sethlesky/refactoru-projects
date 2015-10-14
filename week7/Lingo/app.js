c5ee5c8861d355682a21ec10aa70b1d0f4e05227
Seth Lesky <sethlesky@gmail.com>
2015-10-14 16:07:12 -0600

    Lingo

---
+++ week7/Lingo/app.js
@@ -0,0 +1,25 @@
+// Requires \\
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Create Express App Object \\
+var app = express();
+
+// Application Configuration \\
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+// Routes \\
+app.get('/', function(req, res){
+  // res.send('Hello')
+  res.sendFile('translate.html', {root: './public/html/'});
+
+});
+
+// Creating Server and Listening for Connections \\
+var port = 3000
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
+
+})
