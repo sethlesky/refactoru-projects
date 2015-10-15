fae2c121ba80af06d5dd7d981f1bbb4a2a4c9926
Seth Lesky <sethlesky@gmail.com>
2015-10-15 14:51:02 -0600

    translating server side

--- week7/Lingo/app.js
+++ week7/Lingo/app.js
@@ -1,6 +1,7 @@
 // Requires \\
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 // Create Express App Object \\
 var app = express();
@@ -14,7 +15,20 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
   // res.send('Hello')
   res.sendFile('translate.html', {root: './public/html/'});
+});
 
+app.post('/translate', function(req, res) {
+  // console.log(req.body);
+  request('https://www.googleapis.com/language/translate/v2?q='
+           + req.body.text + '&target=' + req.body.targLang
+           + '&format=text&source=' + req.body.srcLang
+           + '&key=AIzaSyCP5tXQ5CBps_hwVs5s9kQqOg91VVCUjc4'
+    ,function(error, response, body) {
+      if (!error) {
+        console.log(body);
+        res.send(body);
+      }
+    })
 });
 
 // Creating Server and Listening for Connections \\
--- week7/Lingo/public/js/main.js
+++ week7/Lingo/public/js/main.js
@@ -2,21 +2,23 @@
 angular.module('transApp', []);
 
 angular.module('transApp').controller('transCtl', ['$scope', '$http', function($scope, $http) {
-  "https://www.googleapis.com/language/translate/v2?q=goodbye&target=es&format=text&source=en&key=AIzaSyBh0bhARwf6vmHYPwX2tDYYXFjSblAqlWQ";
+  // "https://www.googleapis.com/language/translate/v2?q=goodbye&target=es&format=text&source=en&key=AIzaSyBh0bhARwf6vmHYPwX2tDYYXFjSblAqlWQ";
   $scope.title = "Translate App";
 
-  $scope.trans = {};
+  $scope.trans = {
+    srcLang  : 'en',
+    targLang : 'es',
+    text     : 'hello'
+  };
 
   $scope.translate = function() {
     console.log('translate run', $scope.trans);
 
-    $http.get("https://www.googleapis.com/language/translate/v2?q="
-      + $scope.trans.text + "&target=" + $scope.trans.targLang
-      + "&format=text&source=" + $scope.trans.srcLang
-      + "&key=AIzaSyCP5tXQ5CBps_hwVs5s9kQqOg91VVCUjc4").then(function(data) {
-        console.log(data);
-      })
-
+    $http.post("/translate", $scope.trans)
+      .then(function(result) {
+        console.log(result)
+        $scope.translatedText = result.data.data.translations[0].translatedText;
+      });
   }
 
 
